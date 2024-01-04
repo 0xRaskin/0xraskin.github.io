eaf76621d8e395d2a6f65de0a19ec0a7c718f431
Raskin <0xraskin@protonmail.com>
2024-01-04 16:21:46 +0600

    Update script.js
    ```

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
             // Update dynamic content
             document.getElementById("dynamicContent").innerText = data.html.body.header.h1;
 
-            // Reload the page and redirect after 1.5 seconds
-            setTimeout(function () {
-                // Redirect to another URL
-                window.location.replace("https://x.com/0xRaskin");
-            }, 1500); // Adjust the time as needed
+            // Redirect to another URL
+            window.location.replace("https://x.com/0xRaskin");
         })
         .catch(error => console.error("Error fetching JSON:", error));
 });
